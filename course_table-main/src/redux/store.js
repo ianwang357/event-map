c4d449b9b13b781758e4a73c3933b5007a0dd10e
ianwang357 <91436737+ianwang357@users.noreply.github.com>
2022-04-02 11:58:58 -0500

    First push

--- course_table-main/src/redux/store.js
+++
@@ -1,8 +0,0 @@
-import { configureStore } from '@reduxjs/toolkit';
-import timezoneReducer from './reducer/timezoneReducer';
-
-export const store = configureStore({
-  reducer: {
-    timezone: timezoneReducer,
-  },
-});
